197d9d0c40cc7a406d7afcc5bbb2ed3fc8c5bcbd
chu <iliangchu@gmail.com>
2025-05-26 16:49:24 +0800

    implement training and gameplay functionality for Tic Tac Toe AI

--- main.py
+++ main.py
@@ -48,49 +48,56 @@ class TicTacToeGame:
             tmp = sorted(output1, reverse=True)
             i = 0
             while True:
-                # decision1 = output1.index(tmp[i])
                 try:
                     decision1 = output1.index(tmp[i])
                 except:
                     print(f"i = {i}")
-                    # time.sleep(3)
+                    print(output1)
+                    print(output1.index(tmp[i]))
+                    print(decision1)
+                    time.sleep(30)
                     return
-                    quit()
 
                 result = self.__game.addPieceByNumber(decision1)
                 if result == 0:
+                    output1[output1.index(tmp[i])] = None
                     i += 1
                 else:
                     break
 
+            self.__game.draw()
             if result == 2 or result == 4:
                 self.calculate_fitness(genome1, genome2, result)
-                # time.sleep(3)
                 self.__game.reset()
+                self.__game.draw()
                 return
 
             output2 = net2.activate(self.__game.get_bitBoardList())
             tmp2 = sorted(output2, reverse=True)
             i = 0
             while True:
-                # decision2 = output2.index(tmp2[i])
                 try:
                     decision2 = output2.index(tmp2[i])
                 except:
                     print(f"i = {i}")
-                    # time.sleep(3)
+                    print(output2)
+                    print(output2.index(tmp2[i]))
+                    print(decision2)
+                    time.sleep(30)
                     return
-                    quit()
+
                 result = self.__game.addPieceByNumber(decision2)
                 if result == 0:
+                    output2[output2.index(tmp2[i])] = None
                     i += 1
                 else:
                     break
 
+            self.__game.draw()
             if result == 3 or result == 4:
                 self.calculate_fitness(genome1, genome2, result)
-                # time.sleep(3)
                 self.__game.reset()
+                self.__game.draw()
                 return
 
             self.__game.draw()
@@ -107,31 +114,43 @@ class TicTacToeGame:
         net = neat.nn.FeedForwardNetwork.create(genome, config)
 
         while self.__game.getRun():
+            result = None
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     quit()
                     break
                 if event.type == pygame.MOUSEBUTTONDOWN and self.__game.getRun():
-                    self.__game.addPiece()
+                    result = self.__game.addPiece()
+                    self.__game.draw()
+
+            if result == None:
+                pass
+            elif result == 2 or 4:
+                continue
 
             if self.__game.getCircle() == False:
                 output1 = net.activate(self.__game.get_bitBoardList())
                 tmp = sorted(output1, reverse=True)
                 i = 0
-                # decision1 = output1.index(tmp[i])
-                try:
-                    decision1 = output1.index(tmp[i])
-                except:
-                    print(f"i = {i}")
-                    return
-                    quit()
+                while True:
+                    try:
+                        decision1 = output1.index(tmp[i])
+                    except:
+                        print(f"i = {i}")
+                        print(output1)
+                        print(output1.index(tmp[i]))
+                        print(decision1)
+                        time.sleep(30)
+                        return
 
-                result = self.__game.addPieceByNumber(decision1)
-                if result == 0:
-                    i += 1
-                else:
-                    break
+                    result = self.__game.addPieceByNumber(decision1)
+                    if result == 0:
+                        output1[output1.index(tmp[i])] = None
+                        i += 1
+                    else:
+                        break
 
+                self.__game.draw()
             self.__game.draw()
             pygame.display.update()
 
@@ -139,28 +158,28 @@ class TicTacToeGame:
     
 
 
-# def eval_genomes(genomes, config):
-#     for i, (genome_id1, genome1) in enumerate(genomes):
-#         if i == len(genomes) - 1:
-#             break
-#         genome1.fitness = 0
-#         for genome_id2, genome2 in genomes[i+1:]:
-#             genome2.fitness = 0 if genome2.fitness == None else genome2.fitness
-#             game = TicTacToeGame(WIDTH, HEIGHT)
-#             game.train_ai(genome1, genome2, config)
-#             del game
-
 def eval_genomes(genomes, config):
-    game = TicTacToeGame(WIDTH, HEIGHT)
-    for _ in range(2):
-        for i, (genome_id1, genome1) in enumerate(genomes):
-            if i == len(genomes) - 1:
-                break
-            genome1.fitness = 0
-            genome_id2, genome2 = genomes[i+1]
+    for i, (genome_id1, genome1) in enumerate(genomes):
+        if i == len(genomes) - 1:
+            break
+        genome1.fitness = 0
+        for genome_id2, genome2 in genomes[i+1:]:
             genome2.fitness = 0 if genome2.fitness == None else genome2.fitness
             game = TicTacToeGame(WIDTH, HEIGHT)
             game.train_ai(genome1, genome2, config)
+            del game
+
+# def eval_genomes(genomes, config):
+#     game = TicTacToeGame(WIDTH, HEIGHT)
+#     for _ in range(2):
+#         for i, (genome_id1, genome1) in enumerate(genomes):
+#             if i == len(genomes) - 1:
+#                 break
+#             genome1.fitness = 0
+#             genome_id2, genome2 = genomes[i+1]
+#             genome2.fitness = 0 if genome2.fitness == None else genome2.fitness
+#             game = TicTacToeGame(WIDTH, HEIGHT)
+#             game.train_ai(genome1, genome2, config)
 
 
 def run_neat(config: neat.Config):
@@ -194,5 +213,5 @@ if __name__ == "__main__":
     config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
                          neat.DefaultSpeciesSet, neat.DefaultStagnation,
                          config_path)
-    run_neat(config)
-    # test_ai(config)
+    # run_neat(config)
+    test_ai(config)
